037c995625e6c967b8d4529568aa99cb8c41e51d
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-05 20:57:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,43 @@ end
 end
   
   
+  def won?()
+
+  is_empty = @board.none? { |c| c=="O" || c=="X" }
+ if(is_empty)
+ if((is_array_empty()==true)||(@board == nil))
+  return false
+ else
+  WIN_COMBINATIONS.each do |win_combination|
+    
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+    
+
+     if(((position_1=="X")&&(position_2=="X")&&(position_3=="X"))||((position_1=="O")&&(position_2=="O")&&(position_3=="O")))
+      
+    return win_combination
+
+    end
   
+  end
+  false
+ end
+    else
+      
+      false
+      
+    end
+  
+  end
+ end
+ false
+end
   
   
   
